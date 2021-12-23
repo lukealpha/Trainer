bbe0c0fa1b85ca010ef416fad509d3a3605aab2b
lukealpha <harryli0125lhhl@gmail.com>
2021-12-23 19:37:21 +0800

    Ternary Search P3382

---
+++ P3382.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#define MAXN 15
+using namespace std;
+int n, deg[MAXN];
+int main(){
+    scanf("%d",&n);
+    double l, r;
+    scanf("%lf%lf",&l,&r);
+    for (int i = 0; i <= n; i ++){
+        scanf("%d",&deg[i]);
+    }
+    
+    return 0;
+}
