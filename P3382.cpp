a6b4f2026b03f57fcaa7a721495088e3c1c31024
lukealpha <harryli0125lhhl@gmail.com>
2021-12-23 19:38:51 +0800

    Clean Working Tree

--- P3382.cpp
+++ P3382.cpp
@@ -1,17 +1,37 @@
 #include <cstdio>
 #include <iostream>
 #include <cstring>
+#include <cmath>
 #include <algorithm>
 #define MAXN 15
 using namespace std;
-int n, deg[MAXN];
+
+const double eps = 1e-6;
+int n;
+double deg[MAXN];
+double get_val(double x){
+    double ans = 0;
+    for (int i = 0; i <= n; i ++){
+        ans += (double)deg[i]*pow(x,i);
+    }
+    return ans;
+}
 int main(){
     scanf("%d",&n);
-    double l, r;
+    double l,r;
     scanf("%lf%lf",&l,&r);
-    for (int i = 0; i <= n; i ++){
-        scanf("%d",&deg[i]);
+    l *= 1.000000, r*= 1.000000;
+    for (int i = n; i >= 0; i --){
+        scanf("%lf",&deg[i]);
     }
-    
+    while((r - l) >= eps){
+        double mid = (2*l + r)/3, mmid = (2*r + l)/3;
+        if(get_val(mid) > get_val(mmid)){
+            r = mmid;
+        }
+        else l = mid;
+    }
+    printf("%.5lf",l);
+    system("pause");
     return 0;
 }
