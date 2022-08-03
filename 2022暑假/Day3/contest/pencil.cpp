faf63543f0349eefe89b6355c4a64bef4facfd81
lukealpha <harryli0125lhhl@gmail.com>
2022-08-03 16:35:24 +0800

    Added pencil.cpp

---
+++ 2022暑假/Day3/contest/pencil.cpp
@@ -0,0 +1,49 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#define ll long long
+#define inf 0x3f3f3f3f
+using namespace std;
+const int maxn = 1010;
+const int maxm = 2020;
+int n,m;
+struct edge{
+    int x, y;
+    ll val;
+}e[maxm];
+ll dp[maxn][maxn];
+int main(){
+    scanf("%d%d",&n,&m);
+    for (int i = 1; i <= n; i ++){
+        for (int j = 1; j <= n; j ++){
+            if(i == j){
+                dp[i][j] = 0;
+            }
+            else dp[i][j] = inf;
+        }
+    }
+    for (int i = 1; i <= m; i ++){
+        int x, y;ll val;
+        scanf("%d%d%lld",&x,&y,&val);
+        e[i].x = x, e[i].y = y, e[i].val = val;
+        dp[x][y] = val, dp[y][x] = val;
+    }
+    for (int k = 1; k <= n; k ++){
+        for (int i = 1; i <= n; i ++){
+            for (int j = 1; j <= n; j ++){
+                if(i != k && j != k && i != j){
+                    dp[i][j] = min(dp[i][j],dp[i][k] + dp[k][j]);
+                }
+            }
+        }
+    }
+    for (int i = 1; i <= n; i ++){
+        
+        printf("%lld\n",dp[1][i]+dp[i][n]);
+    }
+    for (int i = 1; i <= m; i ++){
+        printf("%lld\n",dp[1][e[i].x] + dp[e[i].y][n] + e[i].val);
+    }
+    return 0;
+}
